513062e571d30e6b07d9140f9c2a235156dc99bf
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2020-04-17 21:24:08 +0000

    Sol: Fix missing gossip text for Jangdor Swiftstrider

---
+++ data/sql/updates/db_sol_world/2020_04_17_02.sql
@@ -0,0 +1,28 @@
+-- DB update 2020_04_17_01 -> 2020_04_17_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_sol_world' AND COLUMN_NAME = '2020_04_17_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_sol_world CHANGE COLUMN 2020_04_17_01 2020_04_17_02 bit;
+SELECT sql_rev INTO OK FROM version_db_sol_world WHERE sql_rev = '1587158626971525836'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
+INSERT INTO `version_db_sol_world` (`sql_rev`) VALUES ('1587158626971525836');
+
+UPDATE `npc_text` SET `text0_0` = 'Welcome to Camp Mojache, brave $C. How may I assist you this day?' WHERE `ID` = 2368;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
