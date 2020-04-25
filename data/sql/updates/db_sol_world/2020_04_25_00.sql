0c893298a26313ff648be5abcc264089a0f128e2
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2020-04-25 22:25:27 +0000

    Sol: Additional small fix for Anchorite Relic
    - Follow-up 83b99ab934b648b7d1a9c3528dc755575bd629a7
    - Set the Anchorite Relic Bunny active so it will despawn after 5 minutes even if no player is near.

--- data/sql/updates/db_sol_world/2020_04_25_00.sql
+++ data/sql/updates/db_sol_world/2020_04_25_00.sql
@@ -38,9 +38,10 @@ DELETE FROM `smart_scripts` WHERE `entryorguid` = 22444 AND `source_type` = 0;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`)
 VALUES
 (22444,0,0,0,60,0,100,0,500,500,6000,6000,0,11,39184,2,0,0,0,0,1,0,0,0,0,0,0,0,0,'Anchorite Relic Bunny - On Update - Cast ''Anchorite Contrition'''),
-(22444,0,1,2,11,0,100,0,0,0,0,0,0,20,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Anchorite Relic Bunny - On Respawn - Disable Melee'),
-(22444,0,2,3,61,0,100,0,0,0,0,0,0,21,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Anchorite Relic Bunny - Linked - Disable Combat Movement'),
-(22444,0,3,0,61,0,100,0,0,0,0,0,0,97,10,10,1,0,0,0,15,185298,2,0,0,0,0,0,0,'Anchorite Relic Bunny - Linked - Jump To Nearest ''Anchorite Relic''');
+(22444,0,1,2,11,0,100,0,0,0,0,0,0,48,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Anchorite Relic Bunny - On Respawn - Set Active On'),
+(22444,0,2,3,61,0,100,0,0,0,0,0,0,20,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Anchorite Relic Bunny - Linked - Disable Melee'),
+(22444,0,3,4,61,0,100,0,0,0,0,0,0,21,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Anchorite Relic Bunny - Linked - Disable Combat Movement'),
+(22444,0,4,0,61,0,100,0,0,0,0,0,0,97,10,10,1,0,0,0,15,185298,2,0,0,0,0,0,0,'Anchorite Relic Bunny - Linked - Jump To Nearest ''Anchorite Relic''');
 
 --
 -- END UPDATING QUERIES
