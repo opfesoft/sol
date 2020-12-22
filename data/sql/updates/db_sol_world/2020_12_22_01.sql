72671b73f7803b1e3bfe0ac69f8866cf0d40f8d8
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2020-12-22 18:38:04 +0100

    Sol: Small fix concerning Dragonmaw Bonewarder SAI

--- data/sql/updates/db_sol_world/2020_12_22_01.sql
+++ data/sql/updates/db_sol_world/2020_12_22_01.sql
@@ -20,7 +20,7 @@ DELETE FROM `smart_scripts` WHERE `entryorguid` IN (1057,6412) AND `source_type`
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`)
 VALUES
 (1057,0,0,0,25,0,100,1,0,0,0,0,0,11,8853,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Dragonmaw Bonewarder - On Reset - Cast ''Summon Skeleton'' (No Repeat)'),
-(1057,0,1,0,4,0,100,1,1000,1000,0,0,0,11,13787,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Dragonmaw Bonewarder - On Aggro - Cast ''Demon Armor'' (No Repeat)'),
+(1057,0,1,0,4,0,100,1,0,0,0,0,0,11,13787,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Dragonmaw Bonewarder - On Aggro - Cast ''Demon Armor'' (No Repeat)'),
 (1057,0,2,0,0,0,100,0,3000,4000,3000,4000,0,11,6205,32,0,0,0,0,2,0,0,0,0,0,0,0,0,'Dragonmaw Bonewarder - In Combat - Cast ''Curse of Weakness'''),
 (1057,0,3,0,0,0,100,0,0,0,5000,6000,0,11,707,32,0,0,0,0,2,0,0,0,0,0,0,0,0,'Dragonmaw Bonewarder - In Combat - Cast ''Immolate'''),
 
