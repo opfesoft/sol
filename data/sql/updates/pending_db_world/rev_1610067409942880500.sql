30a0e354c392c71ce0878d3eecb6f85be60e8371
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-01-28 14:23:03 +0100

    chore(DB): import pending SQL update file
    Referenced commit(s): 99e0944e309c6628726863db7f0bfaf33edc0138
    
    (cherry picked from commit 878b96aadf84c3d63556c5f5fc0e4c3f327be82f)
    
    Sol modification: Fix update chain

--- data/sql/updates/pending_db_world/rev_1610067409942880500.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1610067409942880500');
-
-UPDATE `creature_template` SET `flags_extra` = (`flags_extra` & ~0x00200000) | 0x40000000 WHERE `flags_extra` & 0x00200000 = 0x00200000;
