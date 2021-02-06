d6f1f391490634adc1f14381868c07bcae65764a
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-06 13:52:53 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 88936e0d1a416e8397a5e788971daf3c371403e2
    
    (cherry picked from commit df83476fa21db3e590bda348eb2f1a10c448511d)

--- data/sql/updates/pending_db_world/rev_1612401387393772200.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1612401387393772200');
-
--- Removing Dupe Veins across lower parts of STV
-DELETE FROM `gameobject` WHERE `guid` IN (12189, 64095, 15430);
