fd37fd4d2f6d5f58f445c6a172b68ca58130feae
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-02-15 09:01:08 +0100

    Sol: Fix some of the Bubbly Fissures
    They were not providing water breathing.

---
+++ data/sql/updates/db_sol_world/2021_02_15_01.sql
@@ -0,0 +1,29 @@
+-- DB update 2021_02_15_00 -> 2021_02_15_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_sol_world' AND COLUMN_NAME = '2021_02_15_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_sol_world CHANGE COLUMN 2021_02_15_00 2021_02_15_01 bit;
+SELECT sql_rev INTO OK FROM version_db_sol_world WHERE sql_rev = '1613376056817494172'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
+INSERT INTO `version_db_sol_world` (`sql_rev`) VALUES ('1613376056817494172');
+
+UPDATE `gameobject_template` SET `type` = 6, `Data1` = 0, `Data2` = 8, `Data3` = 17775, `Data5` = 1 WHERE `entry` = 177524;
+UPDATE `gameobject` SET `id` = 177524, `position_x` = 4006.03, `position_y` = 6006.61 WHERE `guid` = 76930;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
