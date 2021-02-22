9c1992a3891f3078765c96b57b20e5e8f412d0ee
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-02-22 07:50:04 +0100

    Sol: Fix position for Westfall Woodworker
    Spawned in the air near the roof, adjusted to be standing on the roof.

---
+++ data/sql/updates/db_sol_world/2021_02_22_00.sql
@@ -0,0 +1,28 @@
+-- DB update 2021_02_21_01 -> 2021_02_22_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_sol_world' AND COLUMN_NAME = '2021_02_21_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_sol_world CHANGE COLUMN 2021_02_21_01 2021_02_22_00 bit;
+SELECT sql_rev INTO OK FROM version_db_sol_world WHERE sql_rev = '1613976518236499686'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
+INSERT INTO `version_db_sol_world` (`sql_rev`) VALUES ('1613976518236499686');
+
+UPDATE `creature` SET `position_x` = -10633.1, `position_y` = 1186.33, `position_z` = 45.0522, `orientation` = 0.0881376 WHERE `guid` = 45520;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
