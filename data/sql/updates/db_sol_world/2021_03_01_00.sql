db6d4ca02276d65e7b1ebcdb0669df2e698ce67f
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-03-01 09:32:59 +0100

    Sol: Remove SAI for Young Moonkin
    Taken over from AC commit 39ece8d518891197decb10974dc9af6482f467d0

---
+++ data/sql/updates/db_sol_world/2021_03_01_00.sql
@@ -0,0 +1,29 @@
+-- DB update 2021_02_28_00 -> 2021_03_01_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_sol_world' AND COLUMN_NAME = '2021_02_28_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_sol_world CHANGE COLUMN 2021_02_28_00 2021_03_01_00 bit;
+SELECT sql_rev INTO OK FROM version_db_sol_world WHERE sql_rev = '1614587493012826628'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
+INSERT INTO `version_db_sol_world` (`sql_rev`) VALUES ('1614587493012826628');
+
+UPDATE `creature_template` SET `AIName` = '' WHERE  `entry` = 10159;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 10159 AND `source_type` = 0;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
