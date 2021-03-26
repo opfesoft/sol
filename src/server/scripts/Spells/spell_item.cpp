817a40b326b805f7d92a0d755baf5b948c9c9bb5
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-03-26 23:15:35 +0100

    Sol: Fix Nitro Boosts
    Based on the TC version; added a small knockback effect if the actual backfire
    effect is not applicable (no fly area or in dungeons); although this was not
    on retail it's better than applying no backfire effect at all.

--- src/server/scripts/Spells/spell_item.cpp
+++ src/server/scripts/Spells/spell_item.cpp
@@ -3797,31 +3797,36 @@ class spell_item_brewfest_mount_transformation : public SpellScriptLoader
         }
 };
 
-enum NitroBoots
+enum NitroBoosts
 {
-    SPELL_NITRO_BOOTS_SUCCESS       = 54861,
-    SPELL_NITRO_BOOTS_BACKFIRE      = 46014,
+    SPELL_NITRO_BOOSTS_SUCCESS      = 54861,
+    SPELL_NITRO_BOOSTS_BACKFIRE1    = 54621,
+    SPELL_NITRO_BOOSTS_BACKFIRE2    = 54899,
+    SPELL_NITRO_BOOSTS_PARACHUTE    = 54649
 };
 
-class spell_item_nitro_boots : public SpellScriptLoader
+class spell_item_nitro_boosts : public SpellScriptLoader
 {
     public:
-        spell_item_nitro_boots() : SpellScriptLoader("spell_item_nitro_boots") { }
+        spell_item_nitro_boosts() : SpellScriptLoader("spell_item_nitro_boosts") { }
 
-        class spell_item_nitro_boots_SpellScript : public SpellScript
+        class spell_item_nitro_boosts_SpellScript : public SpellScript
         {
-            PrepareSpellScript(spell_item_nitro_boots_SpellScript);
+            PrepareSpellScript(spell_item_nitro_boosts_SpellScript);
 
-            bool Load()
+            bool Load() override
             {
                 if (!GetCastItem())
                     return false;
                 return true;
             }
 
-            bool Validate(SpellInfo const* /*spell*/)
+            bool Validate(SpellInfo const* /*spell*/) override
             {
-                if (!sSpellMgr->GetSpellInfo(SPELL_NITRO_BOOTS_SUCCESS) || !sSpellMgr->GetSpellInfo(SPELL_NITRO_BOOTS_BACKFIRE))
+                if (!sSpellMgr->GetSpellInfo(SPELL_NITRO_BOOSTS_SUCCESS)
+                    || !sSpellMgr->GetSpellInfo(SPELL_NITRO_BOOSTS_BACKFIRE1)
+                    || !sSpellMgr->GetSpellInfo(SPELL_NITRO_BOOSTS_BACKFIRE2)
+                    || !sSpellMgr->GetSpellInfo(SPELL_NITRO_BOOSTS_PARACHUTE))
                     return false;
                 return true;
             }
@@ -3829,18 +3834,76 @@ class spell_item_nitro_boots : public SpellScriptLoader
             void HandleDummy(SpellEffIndex /* effIndex */)
             {
                 Unit* caster = GetCaster();
-                caster->CastSpell(caster, caster->GetMap()->IsDungeon() || roll_chance_i(95) ? SPELL_NITRO_BOOTS_SUCCESS : SPELL_NITRO_BOOTS_BACKFIRE, true, GetCastItem());
+                AreaTableEntry const* areaEntry = sAreaTableStore.LookupEntry(caster->GetAreaId());
+                bool canBackfire = false;
+                if (areaEntry)
+                    if (areaEntry->flags & AREA_FLAG_OUTLAND && !(areaEntry->flags & AREA_FLAG_NO_FLY_ZONE) && !caster->GetMap()->IsDungeon())
+                        canBackfire = true;
+                if (roll_chance_i(95))
+                    caster->CastSpell(caster, SPELL_NITRO_BOOSTS_SUCCESS, true, GetCastItem());
+                else
+                    caster->CastSpell(caster, canBackfire ? SPELL_NITRO_BOOSTS_BACKFIRE1 : SPELL_NITRO_BOOSTS_BACKFIRE2, true, GetCastItem());
             }
 
-            void Register()
+            void Register() override
             {
-                OnEffectHitTarget += SpellEffectFn(spell_item_nitro_boots_SpellScript::HandleDummy, EFFECT_0, SPELL_EFFECT_DUMMY);
+                OnEffectHitTarget += SpellEffectFn(spell_item_nitro_boosts_SpellScript::HandleDummy, EFFECT_0, SPELL_EFFECT_DUMMY);
             }
         };
 
-        SpellScript* GetSpellScript() const
+        SpellScript* GetSpellScript() const override
         {
-            return new spell_item_nitro_boots_SpellScript();
+            return new spell_item_nitro_boosts_SpellScript();
+        }
+};
+
+class spell_item_nitro_boosts_backfire : public SpellScriptLoader
+{
+    public:
+        spell_item_nitro_boosts_backfire() : SpellScriptLoader("spell_item_nitro_boosts_backfire") { }
+
+        class spell_item_nitro_boosts_backfire_AuraScript : public AuraScript
+        {
+            PrepareAuraScript(spell_item_nitro_boosts_backfire_AuraScript);
+
+            bool Validate(SpellInfo const* /*spell*/) override
+            {
+                if (!sSpellMgr->GetSpellInfo(SPELL_NITRO_BOOSTS_PARACHUTE))
+                    return false;
+                return true;
+            }
+
+            void HandleApply(AuraEffect const* /*effect*/, AuraEffectHandleModes /*mode*/)
+            {
+                lastZ = GetTarget()->GetPositionZ();
+            }
+
+            void HandlePeriodicDummy(AuraEffect const* effect)
+            {
+                PreventDefaultAction();
+                float curZ = GetTarget()->GetPositionZ();
+                if (curZ < lastZ)
+                {
+                    if (roll_chance_i(80))
+                        GetTarget()->CastSpell(GetTarget(), SPELL_NITRO_BOOSTS_PARACHUTE, effect);
+                    GetAura()->Remove();
+                }
+                else
+                    lastZ = curZ;
+            }
+
+            void Register() override
+            {
+                OnEffectApply += AuraEffectApplyFn(spell_item_nitro_boosts_backfire_AuraScript::HandleApply, EFFECT_1, SPELL_AURA_PERIODIC_TRIGGER_SPELL, AURA_EFFECT_HANDLE_REAL);
+                OnEffectPeriodic += AuraEffectPeriodicFn(spell_item_nitro_boosts_backfire_AuraScript::HandlePeriodicDummy, EFFECT_1, SPELL_AURA_PERIODIC_TRIGGER_SPELL);
+            }
+
+            float lastZ = INVALID_HEIGHT;
+        };
+
+        AuraScript* GetAuraScript() const
+        {
+            return new spell_item_nitro_boosts_backfire_AuraScript();
         }
 };
 
@@ -4316,7 +4379,8 @@ void AddSC_item_spell_scripts()
     new spell_item_complete_raptor_capture();
     new spell_item_impale_leviroth();
     new spell_item_brewfest_mount_transformation();
-    new spell_item_nitro_boots();
+    new spell_item_nitro_boosts();
+    new spell_item_nitro_boosts_backfire();
     new spell_item_teach_language();
     new spell_item_rocket_boots();
     new spell_item_pygmy_oil();
