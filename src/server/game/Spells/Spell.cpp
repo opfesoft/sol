da88392f98c217acc25f67f5102200a531aa5bad
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-03-26 12:23:06 +0100

    Sol: Implement SPELL_ATTR6_NOT_RESET_SWING_IF_INSTANT
    Taken from TC. For more information see this TC issue:
    https://github.com/TrinityCore/TrinityCore/issues/1765

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -7581,7 +7581,13 @@ bool Spell::IsNextMeleeSwingSpell() const
 bool Spell::IsAutoActionResetSpell() const
 {
     // TODO: changed SPELL_INTERRUPT_FLAG_AUTOATTACK -> SPELL_INTERRUPT_FLAG_INTERRUPT to fix compile - is this check correct at all?
-    return !IsTriggered() && (m_spellInfo->InterruptFlags & SPELL_INTERRUPT_FLAG_INTERRUPT);
+    if (IsTriggered() || !(m_spellInfo->InterruptFlags & SPELL_INTERRUPT_FLAG_INTERRUPT))
+        return false;
+
+    if (!m_casttime && m_spellInfo->HasAttribute(SPELL_ATTR6_NOT_RESET_SWING_IF_INSTANT))
+        return false;
+
+    return true;
 }
 
 bool Spell::IsNeedSendToClient(bool go) const
