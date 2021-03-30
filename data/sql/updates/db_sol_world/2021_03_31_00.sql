80d13a0b592d0977392f7882ecfac45d14dcbb73
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-03-31 00:02:33 +0200

    Sol: Fix position for Dreadmaw Crocolisk
    Also enable random movement

---
+++ data/sql/updates/db_sol_world/2021_03_31_00.sql
@@ -0,0 +1,28 @@
+-- DB update 2021_03_30_02 -> 2021_03_31_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_sol_world' AND COLUMN_NAME = '2021_03_30_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_sol_world CHANGE COLUMN 2021_03_30_02 2021_03_31_00 bit;
+SELECT sql_rev INTO OK FROM version_db_sol_world WHERE sql_rev = '1617141714993907696'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
+INSERT INTO `version_db_sol_world` (`sql_rev`) VALUES ('1617141714993907696');
+
+UPDATE `creature` SET `position_x` = 546.06, `position_y` = -3770.83, `position_z` = 16.8273, `wander_distance` = 10, `MovementType` = 1 WHERE `guid` = 13565;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
