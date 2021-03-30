0952a39cc5ed77e5dad6faad326a1473ba17e66e
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-03-30 23:32:46 +0200

    Sol: Fix position for Giant Clam
    Based on AC commit 7b7dac0512d78538a1737a50d8a4df6c8a9bef79; adjusted orientation

---
+++ data/sql/updates/db_sol_world/2021_03_30_00.sql
@@ -0,0 +1,28 @@
+-- DB update 2021_03_28_00 -> 2021_03_30_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_sol_world' AND COLUMN_NAME = '2021_03_28_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_sol_world CHANGE COLUMN 2021_03_28_00 2021_03_30_00 bit;
+SELECT sql_rev INTO OK FROM version_db_sol_world WHERE sql_rev = '1617139953415059683'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
+INSERT INTO `version_db_sol_world` (`sql_rev`) VALUES ('1617139953415059683');
+
+UPDATE `gameobject` SET `position_x` = 6828.49, `position_y` = -5699.43, `position_z` = -54.5721, `orientation` = 1.52173 WHERE `guid` = 34017;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
