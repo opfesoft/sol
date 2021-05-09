db9d95d91bf9fadbcb0f01ccb1a18215ddcddb18
AzerothCoreBot <azerothcorebot@gmail.com>
2021-05-09 19:10:35 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): d45806737474a402be4bb1bb8fc70c664139e8b7
    
    (cherry picked from commit ad2c87c230d3907a8f25dc1490c59ebcc3f7a464)

--- data/sql/updates/pending_db_world/rev_1620326903465396700.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620326903465396700');
-
-UPDATE `creature_model_info` SET `BoundingRadius` = 1.5, `CombatReach` = 7.5 WHERE `DisplayID` IN (16919,24165);
-
