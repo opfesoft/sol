4e5d387f821b82ba5b0a68665ddd55b740ab91ac
AzerothCoreBot <azerothcorebot@gmail.com>
2021-05-10 00:42:45 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 7e3db47971816c776f635aeda95681e17f138b35
    
    (cherry picked from commit f6d69aef1d173974ceffe36cacb870e7bfcb1c4c)

--- data/sql/updates/pending_db_world/rev_1620411738725445836.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620411738725445836');
-
-UPDATE `gameobject` SET `spawntimesecs`= 7200 WHERE `id` IN (2855,2857,4096,4149,153453,153454,153451,153468);
-UPDATE `gameobject` SET `spawntimesecs`= 36000 WHERE `id` IN (153464,153469);
-
