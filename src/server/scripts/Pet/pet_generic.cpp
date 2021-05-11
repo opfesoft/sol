2619abcac55caeceff0659c75efa95d7beee463b
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-05-11 19:53:40 +0200

    Sol: Fix swimming for Wind Rider Cub & Gryphon Hatchling

--- src/server/scripts/Pet/pet_generic.cpp
+++ src/server/scripts/Pet/pet_generic.cpp
@@ -725,7 +725,7 @@ public:
                 checkTimer2 = 0;
                 if (Unit* owner = me->GetOwner())
                 {
-                    if (owner->HasAuraType(SPELL_AURA_MOD_INCREASE_MOUNTED_FLIGHT_SPEED) || owner->HasAuraType(SPELL_AURA_MOD_INCREASE_MOUNTED_SPEED))
+                    if (owner->HasAuraType(SPELL_AURA_MOD_INCREASE_MOUNTED_FLIGHT_SPEED) || owner->HasAuraType(SPELL_AURA_MOD_INCREASE_MOUNTED_SPEED) || owner->isSwimming())
                     {
                         isFlying = true;
                         me->SetCanFly(true);
