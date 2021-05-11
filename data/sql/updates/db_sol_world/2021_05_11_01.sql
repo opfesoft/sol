308923cfdc3c95ca4693e81b882042c09b97e485
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-05-11 22:10:46 +0200

    Sol: Add equip for Darkspear Dragon Hunter

---
+++ data/sql/updates/db_sol_world/2021_05_11_01.sql
@@ -0,0 +1,35 @@
+-- DB update 2021_05_11_00 -> 2021_05_11_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_sol_world' AND COLUMN_NAME = '2021_05_11_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_sol_world CHANGE COLUMN 2021_05_11_00 2021_05_11_01 bit;
+SELECT sql_rev INTO OK FROM version_db_sol_world WHERE sql_rev = '1620763826386503032'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
+INSERT INTO `version_db_sol_world` (`sql_rev`) VALUES ('1620763826386503032');
+
+UPDATE `creature` SET `equipment_id` = -1 WHERE `id` = 26870;
+
+DELETE FROM `creature_equip_template` WHERE `CreatureID` = 26870;
+INSERT INTO `creature_equip_template` (`CreatureID`, `ID`, `ItemID1`, `ItemID2`, `ItemID3`, `VerifiedBuild`)
+VALUES
+(26870,1,36790,0,0,0),
+(26870,2,36791,0,0,0),
+(26870,3,36792,0,0,0);
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
