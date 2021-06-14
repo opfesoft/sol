2cd4997f0105601913bf593b98d6fd11c56b6794
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-06-14 08:58:41 +0200

    Sol: DB import dfb7e7c58595c0af65292b8704745e2d32860f62

--- data/sql/updates/pending_db_world/rev_1622673575587160400.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1622673575587160400');
-
-UPDATE `quest_template_addon` SET `NextQuestID` = 14353 WHERE `ID` = 14352;
-UPDATE `quest_template_addon` SET `PrevQuestID` = 14352 WHERE `ID` = 14353;
