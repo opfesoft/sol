667f2184ea82e62bd2e3396dbbbe6f4fa719f341
dillyns <49765217+dillyns@users.noreply.github.com>
2021-06-14 17:39:12 -0400

    fix(DB/Creature): Firemane Flamecaller Spawning Inside Wall (#6260)
    Moved the spawn point and added wander
    - Closes #5766
    - Closes chromiecraft/chromiecraft#594
    
    (cherry picked from commit e26dd4bd9204caf10e8894666e6a4128cdb19ad4)
    
    Sol modification: Remove variable

---
+++ data/sql/updates/pending_db_world/rev_1623093110337112400.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1623093110337112400');
+
+UPDATE `creature` 
+SET `position_x` = -4960.708496, `position_y` = -3828.589600, `position_z` = 43.382378, `wander_distance` = 5, `MovementType` = 1 
+WHERE `guid` = 30504;
