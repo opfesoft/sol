1da784b6a8dfb395a2c66e2af183cd7198d28d0b
dillyns <49765217+dillyns@users.noreply.github.com>
2021-06-17 10:11:14 -0400

    fix(DB/Creature): Great Goretusk spawned inside a tree in Redridge Mountains (#6271)
    Moved the spawn point
    - Closes #6116
    - Closes chromiecraft/chromiecraft#729
    
    (cherry picked from commit 947a97933cf7281274df9fc1aebeddcf391a8179)
    
    Sol modification: Remove variable

---
+++ data/sql/updates/pending_db_world/rev_1623161552319933800.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1623161552319933800');
+
+UPDATE `creature` 
+SET `position_x` = -9296.069336, `position_y` = -1887.173462, `position_z` = 78.376251
+WHERE `guid` = 6707;
