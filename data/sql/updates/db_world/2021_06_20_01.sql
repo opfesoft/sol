6b9d5900876bc451cb504e9bc5926028c6760141
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-06-20 22:45:05 +0200

    Sol: DB import 6f8c194bfb2775656f22bb5b05186fea22e8d767

--- data/sql/updates/db_world/2021_06_20_01.sql
+++ data/sql/updates/db_world/2021_06_20_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_06_20_00 -> 2021_06_20_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_06_20_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_06_20_00 2021_06_20_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1623355706619803100'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1623355706619803100');
 
 UPDATE `gameobject_template` SET `AIName` = 'SmartGameObjectAI' WHERE `entry` = 186283;
@@ -9,3 +25,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 
 -- Set Shipwreck Debris gameobject spawntimesecs to 180, it was 900
 UPDATE `gameobject` SET `spawntimesecs` = 180 WHERE `id` = 186283;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
