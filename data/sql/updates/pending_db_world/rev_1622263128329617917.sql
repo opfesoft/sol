3603406b075d527c960710a037c984d1ee58f080
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-06-25 00:44:25 +0200

    Sol: DB import f6dbc94a24448953fd74f1d67f34fdf3a871aa49

--- data/sql/updates/pending_db_world/rev_1622263128329617917.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1622263128329617917');
-
--- Remove Scarlet Gauntlets / Scarlet Wristguard from RLTs
-DELETE FROM `reference_loot_template` WHERE `Item` IN (10331,10333);
