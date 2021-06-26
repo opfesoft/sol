bb11496819e0fe03f40fea6f2624b1d7bea9ae46
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-06-27 00:11:50 +0200

    Sol: Fix rotation for Abercrombie's Crate

---
+++ data/sql/updates/db_sol_world/2021_06_27_00.sql
@@ -0,0 +1,31 @@
+-- DB update 2021_06_24_04 -> 2021_06_27_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_sol_world' AND COLUMN_NAME = '2021_06_24_04';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_sol_world CHANGE COLUMN 2021_06_24_04 2021_06_27_00 bit;
+SELECT sql_rev INTO OK FROM version_db_sol_world WHERE sql_rev = '1624745485656716616'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
+INSERT INTO `version_db_sol_world` (`sql_rev`) VALUES ('1624745485656716616');
+
+DELETE FROM `gameobject_addon` WHERE `guid` = 14137;
+INSERT INTO `gameobject_addon` (`guid`, `invisibilityType`, `invisibilityValue`, `useRotation`)
+VALUES
+(14137,0,0,1);
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
