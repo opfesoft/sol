429b8ce29bd0ce5bb8a243c2b6a7a07364695d2f
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-06-29 07:49:50 +0200

    Sol: Add gossip menu text for Arcanist Helion
    Taken from TC

---
+++ data/sql/updates/db_sol_world/2021_06_29_00.sql
@@ -0,0 +1,33 @@
+-- DB update 2021_06_28_05 -> 2021_06_29_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_sol_world' AND COLUMN_NAME = '2021_06_28_05';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_sol_world CHANGE COLUMN 2021_06_28_05 2021_06_29_00 bit;
+SELECT sql_rev INTO OK FROM version_db_sol_world WHERE sql_rev = '1624945780071544241'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
+INSERT INTO `version_db_sol_world` (`sql_rev`) VALUES ('1624945780071544241');
+
+UPDATE `creature_template` SET `gossip_menu_id` = 6571 WHERE `entry` = 15297;
+
+DELETE FROM `gossip_menu` WHERE `MenuID` = 6571;
+INSERT INTO `gossip_menu` (`MenuID`, `TextID`)
+VALUES
+(6571,7786);
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
