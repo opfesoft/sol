b0a0b5e2ca6858caff94f9fe269c90a2254f7e41
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-06-30 17:34:18 +0200

    Sol: Enable random movement for Greater Duskbat
    Also update the position to be further away from the road.

---
+++ data/sql/updates/db_sol_world/2021_06_30_01.sql
@@ -0,0 +1,28 @@
+-- DB update 2021_06_30_00 -> 2021_06_30_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_sol_world' AND COLUMN_NAME = '2021_06_30_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_sol_world CHANGE COLUMN 2021_06_30_00 2021_06_30_01 bit;
+SELECT sql_rev INTO OK FROM version_db_sol_world WHERE sql_rev = '1625067234142888768'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
+INSERT INTO `version_db_sol_world` (`sql_rev`) VALUES ('1625067234142888768');
+
+UPDATE `creature` SET `position_x` = 2109.85, `position_y` = 819.136, `position_z` = 32.9008, `wander_distance` = 5, `MovementType` = 1 WHERE `guid` = 44593;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
