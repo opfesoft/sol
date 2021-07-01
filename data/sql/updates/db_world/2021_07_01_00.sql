0b8512446b7351b1829a6caf604be6ec492676af
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-07-01 07:48:11 +0200

    Sol: DB import 3071f66743b8329b0c98e0d05498d991823446f8

---
+++ data/sql/updates/db_world/2021_07_01_00.sql
@@ -0,0 +1,30 @@
+-- DB update 2021_06_30_00 -> 2021_07_01_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_06_30_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_06_30_00 2021_07_01_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1621501368347745883'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1621501368347745883');
+
+UPDATE `creature`
+SET `modelid` = 1744, `spawntimesecs` = 275, `wander_distance` = 5, `curhealth` = 178, `movementtype` = 1
+WHERE `guid` BETWEEN 40489 AND 40496 AND `id` = 3254;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
