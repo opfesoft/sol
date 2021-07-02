cb1521d2dab770fed26f43189a5add49a2d4936b
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-07-03 00:56:36 +0200

    Sol: DB import fc82262a4833c34934e345a6f86cf6fb19290376

---
+++ data/sql/updates/db_world/2021_07_03_00.sql
@@ -0,0 +1,29 @@
+-- DB update 2021_07_02_01 -> 2021_07_03_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_07_02_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_07_02_01 2021_07_03_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1624629015111761400'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1624629015111761400');
+
+-- Indurium Mineral Vein - Indurium Ore
+UPDATE `gameobject_loot_template` SET `QuestRequired` = 0 WHERE `Entry` = 3266 AND `Item` = 5833;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
