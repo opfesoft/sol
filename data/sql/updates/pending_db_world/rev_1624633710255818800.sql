ef31f0f9a74f55166c390d4c33076cc614a82302
Alexandru C <alex_cicu94@hotmail.com>
2021-07-02 14:19:45 +0300

    fix(DB/creature): Gray Bear no longer spawned underground  (#6571)
    (cherry picked from commit 358c757966fbdb13d8b39fe76553aa540c1cc878)
    
    Sol modification: Use UPDATE instead of DELETE/INSERT

---
+++ data/sql/updates/pending_db_world/rev_1624633710255818800.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1624633710255818800');
+
+UPDATE `creature` SET `position_x` = 216.317, `position_y` = -991.876, `position_z` = 77.72, `wander_distance` = 5, `MovementType` = 1 WHERE `guid` = 15226;
