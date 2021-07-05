ed66116328ba150147cc20c9e26b6d0b92919d97
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-07-05 23:28:32 +0200

    Sol: DB import d7b03d416b84bb590da4cd0e99159b7ab5302a84

--- data/sql/updates/pending_db_world/rev_1624687532594694252.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1624687532594694252');
-
--- Removes quest requirement from Belamoore's Research Journal junk drop
-UPDATE `creature_loot_template` SET `QuestRequired` = 0 WHERE `Item` = 3711;
