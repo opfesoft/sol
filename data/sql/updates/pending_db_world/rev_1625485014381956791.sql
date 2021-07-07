0657f41e40f007d6a7e478a265fb3e1e5343e916
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-07-08 00:48:40 +0200

    Sol: DB import
    41bb5c4397c2cd4e3c80b4779b5557b52c1c7868
    fcb924a97dff5850fa47319cc6147ad0e9ec5b31
    81f3b62e2fedc740f7ada4aaeaba6c8ca8691ece
    9b2c3405370b414781e5420132f55480be5bd504
    0e1db576bb63dbc2330b67562c4fa3d6205941ed
    3745a595f0e105d60c87dd403180d1699debff50
    9b7b080036f5c5a025617546052371193f51845d
    2c41ba498c091c34bd878d0f2290f3bc3e63d8c5

--- data/sql/updates/pending_db_world/rev_1625485014381956791.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1625485014381956791');
-
--- Relocate Dread Swoop
-UPDATE `creature` SET `position_x` = 52.892, `position_y` = 1563.021, `position_z` = 124.512, `orientation` = 3.459 WHERE `guid` = 27980;
