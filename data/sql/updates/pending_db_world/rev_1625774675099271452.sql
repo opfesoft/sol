4f3a0e257ed674baa77d471eea185fe560682918
Hacki95 <62942638+Hacki95@users.noreply.github.com>
2021-07-10 14:49:37 +0200

    fix(DB): minor issues with Spellshock Leggings (#6834)
    (cherry picked from commit 5020056bfa3779b49464a3764976fda402987b59)
    
    Sol modifications:
    - remove comment update
    - set chance to 0 (means equal chance like the other rare items which also use 0)

---
+++ data/sql/updates/pending_db_world/rev_1625774675099271452.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1625774675099271452');
+
+UPDATE `creature_loot_template` SET `GroupId` = 1, `Chance` = 0 WHERE `Entry` = 7267 AND `Item` = 9484; 
+UPDATE `creature_loot_template` SET `GroupId` = 1, `Chance` = 0 WHERE `Entry` = 8127 AND `Item` = 9484;
+
