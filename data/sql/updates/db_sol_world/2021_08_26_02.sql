ea6a5f28d1d4da323740da0d033030b93fcaf994
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-08-26 08:37:50 +0200

    Sol: Fix position & movement for Primitive Owlbeast

---
+++ data/sql/updates/db_sol_world/2021_08_26_02.sql
@@ -0,0 +1,28 @@
+-- DB update 2021_08_26_01 -> 2021_08_26_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_sol_world' AND COLUMN_NAME = '2021_08_26_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_sol_world CHANGE COLUMN 2021_08_26_01 2021_08_26_02 bit;
+SELECT sql_rev INTO OK FROM version_db_sol_world WHERE sql_rev = '1629959839782086648'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
+INSERT INTO `version_db_sol_world` (`sql_rev`) VALUES ('1629959839782086648');
+
+UPDATE `creature` SET `position_x` = 167.862, `position_y` = -3543.3, `position_z` = 127.854, `wander_distance` = 10, MovementType = 1 WHERE `guid` = 92955;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
