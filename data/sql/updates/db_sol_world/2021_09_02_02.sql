f77765cde1d37e41a2837f231a4c4e10080d19f3
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-09-02 14:58:45 +0200

    Sol: Improve SAI for Ghostclaw Lynx
    Trigger their spells because otherwise they are not usable (need energy)

---
+++ data/sql/updates/db_sol_world/2021_09_02_02.sql
@@ -0,0 +1,28 @@
+-- DB update 2021_09_02_01 -> 2021_09_02_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_sol_world' AND COLUMN_NAME = '2021_09_02_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_sol_world CHANGE COLUMN 2021_09_02_01 2021_09_02_02 bit;
+SELECT sql_rev INTO OK FROM version_db_sol_world WHERE sql_rev = '1630587508195822246'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
+INSERT INTO `version_db_sol_world` (`sql_rev`) VALUES ('1630587508195822246');
+
+UPDATE `smart_scripts` SET `action_param2` = 2 WHERE `entryorguid` = 16348 AND `source_type` = 0 AND `id` IN (0,1);
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
