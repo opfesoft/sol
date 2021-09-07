cec4b39c9ae2e7ef3aea6cff35144dc1433dfa21
Azcobu <81782124+Azcobu@users.noreply.github.com>
2021-09-08 05:20:10 +0930

    fix(DB/creature_loot): remove Frost Oil drops from 3 NPCs (#7745)
    (cherry picked from commit 77f118951a6a42e715824463e4cd378883a3d86c)

---
+++ data/sql/updates/db_world/2021_09_08_00.sql
@@ -0,0 +1,30 @@
+-- DB update 2021_09_07_04 -> 2021_09_08_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_09_07_04';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_09_07_04 2021_09_08_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1630927565231100667'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1630927565231100667');
+
+-- Remove Frost Oil from Vilebranch Witch Doctor, Tar Creeper, and Deadwood Gardener
+DELETE FROM `creature_loot_template` WHERE `Entry` IN (2640, 6527, 7154) AND `Item` = 3829;
+
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
