35511b84d9bf254cd592121137d9cc7708393883
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-09-18 08:44:09 +0200

    Sol: Fix rotation for GO "Black Shield"

---
+++ data/sql/updates/db_sol_world/2021_09_18_00.sql
@@ -0,0 +1,31 @@
+-- DB update 2021_09_17_04 -> 2021_09_18_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_sol_world' AND COLUMN_NAME = '2021_09_17_04';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_sol_world CHANGE COLUMN 2021_09_17_04 2021_09_18_00 bit;
+SELECT sql_rev INTO OK FROM version_db_sol_world WHERE sql_rev = '1631947415531895167'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
+INSERT INTO `version_db_sol_world` (`sql_rev`) VALUES ('1631947415531895167');
+
+DELETE FROM `gameobject_addon` WHERE `guid` = 6821;
+INSERT INTO `gameobject_addon` (`guid`, `invisibilityType`, `invisibilityValue`, `useRotation`)
+VALUES
+(6821,0,0,1);
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
