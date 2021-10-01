36b16d65e2fa3e993ace01175c4f09e10b412971
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-10-01 23:24:21 +0200

    Sol: Fix resting XP in-game
    Taken from AC commit cff7cc58ff6ae879277dc4e306db535951b24c43

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -1588,6 +1588,7 @@ void Player::Update(uint32 p_time)
         }
     }
 
+    time_t lastTick = m_Last_tick;
     if (now > m_Last_tick)
     {
         // Update items that have just a limited lifetime
@@ -1711,7 +1712,7 @@ void Player::Update(uint32 p_time)
 
     if (HasFlag(PLAYER_FLAGS, PLAYER_FLAGS_RESTING))
     {
-        if (now > m_Last_tick && _restTime > 0)             // freeze update
+        if (now > lastTick && _restTime > 0)             // freeze update
         {
             time_t currTime = time(NULL);
             time_t timeDiff = currTime - _restTime;
