5c2aa2a2f10f00e34c16050c1019d614b8f53da4
UltraNix <80540499+UltraNix@users.noreply.github.com>
2021-10-11 00:05:09 +0200

    fix(DB/Creature): Fixed gossip for Innkeeper Pala. (#8181)
    * closes #8089
    
    (cherry picked from commit 091f8d7145259207782a06eb9bec37bf3268e55f)
    
    Sol modification: Adjust comments

---
+++ data/sql/updates/db_world/2021_10_11_04.sql
@@ -0,0 +1,31 @@
+-- DB update 2021_10_11_03 -> 2021_10_11_04
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_10_11_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_10_11_03 2021_10_11_04 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1633030444663834900'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633030444663834900');
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=6746 AND `id`>1;
+INSERT INTO `smart_scripts` VALUES
+(6746,0,2,3,62,0,100,0,21215,0,0,0,0,72,0,0,0,0,0,0,7,0,0,0,0,0,0,0,0,'Innkeeper Pala - On Gossip Option 0 Selected - Close Gossip'),
+(6746,0,3,0,61,0,100,0,0,0,0,0,0,85,24751,0,0,0,0,0,7,0,0,0,0,0,0,0,0,'Innkeeper Pala - Linked - Invoker Cast ''Trick or Treat''');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
