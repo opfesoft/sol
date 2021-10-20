ee5567909f8780c3674db11d1e673680d652c132
Azcobu <81782124+Azcobu@users.noreply.github.com>
2021-10-20 19:03:26 +1030

    fix(DB/quest): Sets Apothecary Dithers as quest start/end for "The Key to Scholomance" (#8552)
    (cherry picked from commit a1126c18c4c8cd85f06ae04fe7170c4a64a6ba2d)

---
+++ data/sql/updates/db_world/2021_10_20_07.sql
@@ -0,0 +1,31 @@
+-- DB update 2021_10_20_06 -> 2021_10_20_07
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_10_20_06';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_10_20_06 2021_10_20_07 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1634356384710144357'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634356384710144357');
+
+-- Makes Apothecary Dithers start and end "The Key to Scholomance"
+UPDATE `creature_queststarter` SET `id` = 11057 WHERE `quest` = 5511;
+UPDATE `creature_questender` SET `id` = 11057 WHERE `quest` = 5511;
+
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
