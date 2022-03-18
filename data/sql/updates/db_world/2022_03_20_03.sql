2136b79c12feed7da7ea3723974ac7f83f40fe0a
Kosiak <v.k.kosiak@gmail.com>
2022-03-18 23:30:51 +0000

    fix(DB/creature_template): Love is in the Air Creatures Level (#11009)
    (cherry picked from commit 534056f5615747e5f29e01c75ea6e508f20f6a85)

---
+++ data/sql/updates/db_world/2022_03_20_03.sql
@@ -0,0 +1,29 @@
+-- DB update 2022_03_20_02 -> 2022_03_20_03
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_03_20_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_03_20_02 2022_03_20_03 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1647208425086920700'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647208425086920700');
+
+UPDATE `creature_template` SET `minlevel` = 25, `maxlevel` = 26 WHERE `entry` = 37984;
+UPDATE `creature_template` SET `minlevel` = 33, `maxlevel` = 34 WHERE `entry` = 38006;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
