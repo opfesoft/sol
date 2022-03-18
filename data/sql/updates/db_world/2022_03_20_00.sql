79da3f7c68cae5e3cc2b7c81972e1f27250054b0
Kosiak <v.k.kosiak@gmail.com>
2022-03-18 21:55:19 +0000

    fix(DB/quest_template): Rat Catching (#11008)
    (cherry picked from commit a067b4cf5c6eeab8d8863ce6ca38a268806f92b4)

---
+++ data/sql/updates/db_world/2022_03_20_00.sql
@@ -0,0 +1,28 @@
+-- DB update 2022_03_17_00 -> 2022_03_20_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_03_17_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_03_17_00 2022_03_20_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1647204744919951000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647204744919951000');
+
+UPDATE `quest_template` SET `QuestCompletionLog` = 'Return to Mountaineer Kadrell at Thelsamar in Loch Modan.' WHERE `ID` = 416;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
