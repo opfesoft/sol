a2322966da19214a2660d8d7553c16d0b2705631
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2022-05-15 19:45:02 +0200

    Sol: Allow mount in Shadow Dance form

--- src/server/game/Entities/Unit/Unit.h
+++ src/server/game/Entities/Unit/Unit.h
@@ -2085,7 +2085,7 @@ class Unit : public WorldObject
         {
             ShapeshiftForm form = GetShapeshiftForm();
             return form != FORM_NONE && form != FORM_BATTLESTANCE && form != FORM_BERSERKERSTANCE && form != FORM_DEFENSIVESTANCE &&
-                form != FORM_SHADOW && form != FORM_STEALTH && form != FORM_UNDEAD;
+                form != FORM_SHADOW && form != FORM_STEALTH && form != FORM_UNDEAD && form != FORM_SHADOW_DANCE;
         }
 
         float m_modMeleeHitChance;
