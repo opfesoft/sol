3d33fdfeae14ca4313b6254a6e0794cf768d51df
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2022-05-26 10:59:57 +0200

    Sol: Fix creature text for Goblin Land Mine
    Taken from the TC DB.

---
+++ data/sql/updates/db_world/2022_05_26_00.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_text` SET `BroadcastTextId` = 3541 WHERE `CreatureID` = 7527 AND `GroupID` = 0;
+UPDATE `creature_text` SET `Sound` = 0, `BroadcastTextId` = 3542 WHERE `CreatureID` = 7527 AND `GroupID` = 1;
+UPDATE `creature_text` SET `BroadcastTextId` = 3543 WHERE `CreatureID` = 7527 AND `GroupID` = 2;
