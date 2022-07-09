araxiaonline/AzerothCore-wotlk-with-NPCBots

feat(Core/Commands): Debug Spell Visuals Command (#12216)
* feat(Core/Commands): Debug Spell Visuals Command

* feat(DB/Commands): Debug Visual Help

* Add missing eol at eof

src/server/scripts/Commands/cs_debug.cpp
@@ -58,7 +58,8 @@ public:
             { "cinematic",      HandleDebugPlayCinematicCommand,       SEC_ADMINISTRATOR, Console::No },
             { "movie",          HandleDebugPlayMovieCommand,           SEC_ADMINISTRATOR, Console::No },
             { "sound",          HandleDebugPlaySoundCommand,           SEC_ADMINISTRATOR, Console::No },
-            { "music",          HandleDebugPlayMusicCommand,           SEC_ADMINISTRATOR, Console::No }
+            { "music",          HandleDebugPlayMusicCommand,           SEC_ADMINISTRATOR, Console::No },
+            { "visual",         HandleDebugVisualCommand,              SEC_ADMINISTRATOR, Console::No }
         };
         static ChatCommandTable debugSendCommandTable =
         {
@@ -200,6 +201,28 @@ public:
         player->PlayDirectMusic(musicId, player);
 
         handler->PSendSysMessage(LANG_YOU_HEAR_SOUND, musicId);
+        return true;
+    }
+
+    static bool HandleDebugVisualCommand(ChatHandler* handler, uint32 visualId)
+    {
+        if (!visualId)
+        {
+            handler->SendSysMessage(LANG_BAD_VALUE);
+            handler->SetSentErrorMessage(true);
+            return false;
+        }
+
+        Player* player = handler->GetPlayer();
+        Unit* target = handler->getSelectedUnit();
+
+        if (!target)
+        {
+            player->SendPlaySpellVisual(visualId);
+            return true;
+        }
+
+        player->SendPlaySpellImpact(target->GetGUID(), visualId);
         return true;
     }
 

opfesoft/sol

(cherry picked from commit c93fb05515dd1768cda9285154b7916b2c30c4d3)
Adapted to Sol

src/server/scripts/Commands/cs_debug.cpp
@@ -37,7 +37,8 @@ public:
         {
             { "cinematic",      SEC_MODERATOR,      false, &HandleDebugPlayCinematicCommand,   "" },
             { "movie",          SEC_MODERATOR,      false, &HandleDebugPlayMovieCommand,       "" },
-            { "sound",          SEC_MODERATOR,      false, &HandleDebugPlaySoundCommand,       "" }
+            { "sound",          SEC_MODERATOR,      false, &HandleDebugPlaySoundCommand,       "" },
+            { "visual",         SEC_MODERATOR,      false, &HandleDebugVisualCommand,          "" }
         };
         static std::vector<ChatCommand> debugSendCommandTable =
         {
@@ -174,6 +175,26 @@ public:
             unit->PlayDirectSound(soundId, handler->GetSession()->GetPlayer());
 
         handler->PSendSysMessage(LANG_YOU_HEAR_SOUND, soundId);
+        return true;
+    }
+
+    static bool HandleDebugVisualCommand(ChatHandler* handler, char const* args)
+    {
+        if (!*args)
+            return false;
+
+        uint32 visualId = atoi((char*)args);
+
+        Unit* unit = handler->getSelectedUnit();
+
+        if (!unit)
+        {
+            handler->SendSysMessage(LANG_SELECT_CHAR_OR_CREATURE);
+            handler->SetSentErrorMessage(true);
+            return false;
+        }
+
+        unit->SendPlaySpellVisual(visualId);
         return true;
     }
 