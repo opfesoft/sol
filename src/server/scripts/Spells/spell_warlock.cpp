araxiaonline/AzerothCore-wotlk-with-NPCBots

fix(Scripts/Spells): Glyph of Felguard. (#12497)
* fix(Scripts/Spells): Glyph of Felguard.

Fixes #12432

* Update.

src/server/scripts/Spells/spell_warlock.cpp
@@ -21,6 +21,7 @@
  * Scriptnames of files in this file should be prefixed with "spell_warl_".
  */
 
+#include "Pet.h"
 #include "Player.h"
 #include "ScriptMgr.h"
 #include "SpellAuraEffects.h"
@@ -1229,6 +1230,45 @@ class spell_warl_shadowburn : public AuraScript
     }
 };
 
+class spell_warl_glyph_of_felguard : public AuraScript
+{
+    PrepareAuraScript(spell_warl_glyph_of_felguard);
+
+    void HandleApply(AuraEffect const* aurEff, AuraEffectHandleModes /*mode*/)
+    {
+        if (Player* player = GetCaster()->ToPlayer())
+        {
+            if (Pet* pet = player->GetPet())
+            {
+                if (pet->GetEntry() == NPC_FELGUARD)
+                {
+                    pet->HandleStatModifier(UNIT_MOD_ATTACK_POWER, TOTAL_PCT, aurEff->GetAmount(), true);
+                }
+            }
+        }
+    }
+
+    void HandleRemove(AuraEffect const* aurEff, AuraEffectHandleModes /*mode*/)
+    {
+        if (Player* player = GetCaster()->ToPlayer())
+        {
+            if (Pet* pet = player->GetPet())
+            {
+                if (pet->GetEntry() == NPC_FELGUARD)
+                {
+                    pet->HandleStatModifier(UNIT_MOD_ATTACK_POWER, TOTAL_PCT, aurEff->GetAmount(), false);
+                }
+            }
+        }
+    }
+
+    void Register() override
+    {
+        OnEffectApply += AuraEffectApplyFn(spell_warl_glyph_of_felguard::HandleApply, EFFECT_0, SPELL_AURA_DUMMY, AURA_EFFECT_HANDLE_REAL);
+        OnEffectRemove += AuraEffectRemoveFn(spell_warl_glyph_of_felguard::HandleRemove, EFFECT_0, SPELL_AURA_DUMMY, AURA_EFFECT_HANDLE_REAL);
+    }
+};
+
 void AddSC_warlock_spell_scripts()
 {
     RegisterSpellScript(spell_warl_eye_of_kilrogg);
@@ -1260,4 +1300,5 @@ void AddSC_warlock_spell_scripts()
     RegisterSpellScript(spell_warl_unstable_affliction);
     RegisterSpellScript(spell_warl_drain_soul);
     RegisterSpellScript(spell_warl_shadowburn);
+    RegisterSpellScript(spell_warl_glyph_of_felguard);
 }

opfesoft/sol

(cherry picked from commit b81a14976cd2922f5b95948af7d85014287d5c54)
Adapted to Sol

src/server/scripts/Spells/spell_warlock.cpp
@@ -17,6 +17,7 @@
 #include "Player.h"
 #include "SpellInfo.h"
 #include "TemporarySummon.h"
+#include "Pet.h"
 
 enum WarlockSpells
 {
@@ -1391,6 +1392,45 @@ class spell_warl_glyph_of_shadowflame : public SpellScriptLoader
         }
 };
 
+// 56246 - Glyph of Felguard
+class spell_warl_glyph_of_felguard : public SpellScriptLoader
+{
+    public:
+        spell_warl_glyph_of_felguard() : SpellScriptLoader("spell_warl_glyph_of_felguard") { }
+
+        class spell_warl_glyph_of_felguard_AuraScript : public AuraScript
+        {
+            PrepareAuraScript(spell_warl_glyph_of_felguard_AuraScript);
+
+            void HandleApply(AuraEffect const* aurEff, AuraEffectHandleModes /*mode*/)
+            {
+                if (Player* player = GetCaster()->ToPlayer())
+                    if (Pet* pet = player->GetPet())
+                        if (pet->GetEntry() == NPC_FELGUARD)
+                            pet->HandleStatModifier(UNIT_MOD_ATTACK_POWER, TOTAL_PCT, aurEff->GetAmount(), true);
+            }
+
+            void HandleRemove(AuraEffect const* aurEff, AuraEffectHandleModes /*mode*/)
+            {
+                if (Player* player = GetCaster()->ToPlayer())
+                    if (Pet* pet = player->GetPet())
+                        if (pet->GetEntry() == NPC_FELGUARD)
+                            pet->HandleStatModifier(UNIT_MOD_ATTACK_POWER, TOTAL_PCT, aurEff->GetAmount(), false);
+            }
+
+            void Register()
+            {
+                OnEffectApply += AuraEffectApplyFn(spell_warl_glyph_of_felguard_AuraScript::HandleApply, EFFECT_0, SPELL_AURA_DUMMY, AURA_EFFECT_HANDLE_REAL);
+                OnEffectRemove += AuraEffectRemoveFn(spell_warl_glyph_of_felguard_AuraScript::HandleRemove, EFFECT_0, SPELL_AURA_DUMMY, AURA_EFFECT_HANDLE_REAL);
+            }
+        };
+
+        AuraScript* GetAuraScript() const
+        {
+            return new spell_warl_glyph_of_felguard_AuraScript();
+        }
+};
+
 void AddSC_warlock_spell_scripts()
 {
     // Ours
@@ -1403,6 +1443,7 @@ void AddSC_warlock_spell_scripts()
     new spell_warl_demonic_knowledge();
     new spell_warl_generic_scaling();
     new spell_warl_infernal_scaling();
+    new spell_warl_glyph_of_felguard();
 
     // Theirs
     new spell_warl_banish();