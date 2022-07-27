04e0d234d715cd9a1468c5935c4c0937146d78e7
UltraNix <80540499+UltraNix@users.noreply.github.com>
2022-07-27 14:41:24 +0200

    fix(DB/Spells): Greater Heal spell from 8P T2 Priest set should scale… (#12504)
    * fix(DB/Spells): Greater Heal spell from 8P T2 Priest set should scale with spell power.
    
    Fixes #11934
    
    * Update.
    
    (cherry picked from commit 9ea048b46071ae38cb184bbdbf0bcb5c670830f8)
    
    Sol modification: Add column names for insert

---
+++ data/sql/updates/db_world/2022_07_29_00.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_bonus_data` WHERE `entry`=22009;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES
+(22009,0,0.2,0,0,'Priest - Greater Heal - 8P T2');
