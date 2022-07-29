05a8a40ba8e6550adb443f52008fc8dd5d6ab32e
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2022-07-29 09:48:25 +0200

    Sol: Fix proc for Lightning/Earth/Water Shield
    Do not proc if actor and action target is self.

---
+++ data/sql/updates/db_world/2022_07_29_01.sql
@@ -0,0 +1,30 @@
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 24 AND `SourceEntry` IN (324,325,905,945,8134,10431,10432,25469,25472,49280,49281,974,32593,32594,49283,49284,52127,52129,52131,52134,52136,52138,24398,33736,57960);
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `Comment`)
+VALUES
+(24,0,324,0,0,33,0,1,0,0,1,0,0,'Lightning Shield (Rank 1) - Group 0: Proc only if actor and action target is not self'),
+(24,0,325,0,0,33,0,1,0,0,1,0,0,'Lightning Shield (Rank 2) - Group 0: Proc only if actor and action target is not self'),
+(24,0,905,0,0,33,0,1,0,0,1,0,0,'Lightning Shield (Rank 3) - Group 0: Proc only if actor and action target is not self'),
+(24,0,945,0,0,33,0,1,0,0,1,0,0,'Lightning Shield (Rank 4) - Group 0: Proc only if actor and action target is not self'),
+(24,0,8134,0,0,33,0,1,0,0,1,0,0,'Lightning Shield (Rank 5) - Group 0: Proc only if actor and action target is not self'),
+(24,0,10431,0,0,33,0,1,0,0,1,0,0,'Lightning Shield (Rank 6) - Group 0: Proc only if actor and action target is not self'),
+(24,0,10432,0,0,33,0,1,0,0,1,0,0,'Lightning Shield (Rank 7) - Group 0: Proc only if actor and action target is not self'),
+(24,0,25469,0,0,33,0,1,0,0,1,0,0,'Lightning Shield (Rank 8) - Group 0: Proc only if actor and action target is not self'),
+(24,0,25472,0,0,33,0,1,0,0,1,0,0,'Lightning Shield (Rank 9) - Group 0: Proc only if actor and action target is not self'),
+(24,0,49280,0,0,33,0,1,0,0,1,0,0,'Lightning Shield (Rank 10) - Group 0: Proc only if actor and action target is not self'),
+(24,0,49281,0,0,33,0,1,0,0,1,0,0,'Lightning Shield (Rank 11) - Group 0: Proc only if actor and action target is not self'),
+
+(24,0,974,0,0,33,0,1,0,0,1,0,0,'Earth Shield (Rank 1) - Group 0: Proc only if actor and action target is not self'),
+(24,0,32593,0,0,33,0,1,0,0,1,0,0,'Earth Shield (Rank 2) - Group 0: Proc only if actor and action target is not self'),
+(24,0,32594,0,0,33,0,1,0,0,1,0,0,'Earth Shield (Rank 3) - Group 0: Proc only if actor and action target is not self'),
+(24,0,49283,0,0,33,0,1,0,0,1,0,0,'Earth Shield (Rank 4) - Group 0: Proc only if actor and action target is not self'),
+(24,0,49284,0,0,33,0,1,0,0,1,0,0,'Earth Shield (Rank 5) - Group 0: Proc only if actor and action target is not self'),
+
+(24,0,52127,0,0,33,0,1,0,0,1,0,0,'Water Shield (Rank 1) - Group 0: Proc only if actor and action target is not self'),
+(24,0,52129,0,0,33,0,1,0,0,1,0,0,'Water Shield (Rank 2) - Group 0: Proc only if actor and action target is not self'),
+(24,0,52131,0,0,33,0,1,0,0,1,0,0,'Water Shield (Rank 3) - Group 0: Proc only if actor and action target is not self'),
+(24,0,52134,0,0,33,0,1,0,0,1,0,0,'Water Shield (Rank 4) - Group 0: Proc only if actor and action target is not self'),
+(24,0,52136,0,0,33,0,1,0,0,1,0,0,'Water Shield (Rank 5) - Group 0: Proc only if actor and action target is not self'),
+(24,0,52138,0,0,33,0,1,0,0,1,0,0,'Water Shield (Rank 6) - Group 0: Proc only if actor and action target is not self'),
+(24,0,24398,0,0,33,0,1,0,0,1,0,0,'Water Shield (Rank 7) - Group 0: Proc only if actor and action target is not self'),
+(24,0,33736,0,0,33,0,1,0,0,1,0,0,'Water Shield (Rank 8) - Group 0: Proc only if actor and action target is not self'),
+(24,0,57960,0,0,33,0,1,0,0,1,0,0,'Water Shield (Rank 9) - Group 0: Proc only if actor and action target is not self');
