696a028257d0d50113777441653b2065000a19de
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2022-09-06 20:05:13 -0400

    fix(DB/creature): Correct Newman's Landing NPC positions and vendor items. (#12914)
    (cherry picked from commit 54fb944948e668dc673caf98094c7d92851bec56)
    
    Sol modificaiton: Remove "npc_vendor" change

---
+++ data/sql/updates/db_world/2022_09_07_02.sql
@@ -0,0 +1,5 @@
+UPDATE `creature_equip_template` SET `ItemID1` = 19022 WHERE  `CreatureID` = 26081;
+
+UPDATE `creature` SET `position_x` = -6378.61, `position_y` = 1263.21, `position_z` = 7.2709, `orientation` = 3.03687 WHERE `guid` = 1975958;
+UPDATE `creature` SET `position_x` = -6373.53, `position_y` = 1267.88, `position_z` = 7.2709, `orientation` = 2.39110 WHERE `guid` = 1975959;
+UPDATE `creature` SET `position_x` = -6378.08, `position_y` = 1268.47, `position_z` = 7.2709, `orientation` = 2.44346 WHERE `guid` = 1975960;
