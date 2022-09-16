d73421a924766946b77b0756ce13d6ead3bd47a2
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2022-09-16 19:55:55 +0200

    Sol: Fix Judgement Armor 8P bonus

---
+++ data/sql/updates/db_world/2022_09_16_02.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_proc_event` WHERE `entry` = 23591;
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`)
+VALUES
+(23591,0,10,8388608,0,0,16,0,0,0,0);
