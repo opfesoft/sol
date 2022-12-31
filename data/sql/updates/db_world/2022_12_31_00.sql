f080f43423ee89f0f280d258caa65f8df19a213d
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2022-12-31 09:40:13 +0100

    Sol: Add NPC "Ludin Farrow"
    Based on TC commit caf6d6721a077b8ff6e8402a667a4789905551f3

---
+++ data/sql/updates/db_world/2022_12_31_00.sql
@@ -0,0 +1,4 @@
+DELETE FROM `creature` WHERE `guid` = 113557;
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`)
+VALUES
+(113557,26546,0,0,0,1,1,0,1,-3726.61,-576.766,4.64331,4.17134,300,0,1,0,0,0,0,0,0);
