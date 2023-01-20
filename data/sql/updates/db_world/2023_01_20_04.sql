e1c6061c42b801cf915f486cc66a7ea7e4766668
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2023-01-20 16:08:48 +0100

    Sol: Update position for 2 Bonelashers and enable random movement

---
+++ data/sql/updates/db_world/2023_01_20_04.sql
@@ -0,0 +1,2 @@
+UPDATE `creature` SET `position_x` = -3395.53, `position_y` = 4590.61, `position_z` = -21.8332, `wander_distance` = 5, `MovementType` = 1 WHERE `guid` = 66458;
+UPDATE `creature` SET `position_x` = -3439.2, `position_y` = 4651.88, `position_z` = -22.3495, `wander_distance` = 5, `MovementType` = 1 WHERE `guid` = 66459;
