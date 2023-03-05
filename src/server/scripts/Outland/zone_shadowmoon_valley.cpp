c36eaa91b35f03e8d8871e5570f4375c5d8af179
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2023-03-05 23:47:52 +0100

    Sol: Replace creature script "npc_enraged_spirit" with SAI

--- src/server/scripts/Outland/zone_shadowmoon_valley.cpp
+++ src/server/scripts/Outland/zone_shadowmoon_valley.cpp
@@ -24,7 +24,6 @@ npc_torloth_the_magnificent
 npc_illidari_spawn
 npc_lord_illidan_stormrage
 go_crystal_prison
-npc_enraged_spirit
 EndContentData */
 
 #include "ScriptMgr.h"
@@ -1756,131 +1755,6 @@ public:
     }
 };
 
-/*####
-# npc_enraged_spirits
-####*/
-
-enum Enraged_Dpirits
-{
-// QUESTS
-    QUEST_ENRAGED_SPIRITS_FIRE_EARTH        = 10458,
-    QUEST_ENRAGED_SPIRITS_AIR               = 10481,
-    QUEST_ENRAGED_SPIRITS_WATER             = 10480,
-
-    // Totem
-    ENTRY_TOTEM_OF_SPIRITS                  = 21071,
-    RADIUS_TOTEM_OF_SPIRITS                 = 15,
-
-    // SPIRITS
-    NPC_ENRAGED_EARTH_SPIRIT                = 21050,
-    NPC_ENRAGED_FIRE_SPIRIT                 = 21061,
-    NPC_ENRAGED_AIR_SPIRIT                  = 21060,
-    NPC_ENRAGED_WATER_SPIRIT                = 21059,
-
-    // SOULS
-    NPC_EARTHEN_SOUL                        = 21073,
-    NPC_FIERY_SOUL                          = 21097,
-    NPC_ENRAGED_AIRY_SOUL                   = 21116,
-    NPC_ENRAGED_WATERY_SOUL                 = 21109, // wrong model
-
-    // SPELL KILLCREDIT - not working!?! - using KilledMonsterCredit
-    SPELL_EARTHEN_SOUL_CAPTURED_CREDIT      = 36108,
-    SPELL_FIERY_SOUL_CAPTURED_CREDIT        = 36117,
-    SPELL_AIRY_SOUL_CAPTURED_CREDIT         = 36182,
-    SPELL_WATERY_SOUL_CAPTURED_CREDIT       = 36171,
-
-    // KilledMonsterCredit Workaround
-    NPC_CREDIT_FIRE                         = 21094,
-    NPC_CREDIT_WATER                        = 21095,
-    NPC_CREDIT_AIR                          = 21096,
-    NPC_CREDIT_EARTH                        = 21092,
-
-    // Captured Spell / Buff
-    SPELL_SOUL_CAPTURED                     = 36115,
-
-    // Factions
-    FACTION_ENRAGED_SOUL_FRIENDLY           = 35,
-    FACTION_ENRAGED_SOUL_HOSTILE            = 14
-};
-
-class npc_enraged_spirit : public CreatureScript
-{
-public:
-    npc_enraged_spirit() : CreatureScript("npc_enraged_spirit") { }
-
-    CreatureAI* GetAI(Creature* creature) const
-    {
-    return new npc_enraged_spiritAI(creature);
-    }
-
-    struct npc_enraged_spiritAI : public ScriptedAI
-    {
-        npc_enraged_spiritAI(Creature* creature) : ScriptedAI(creature) { }
-
-        void Reset() { }
-
-        void EnterCombat(Unit* /*who*/) { }
-
-        void JustDied(Unit* /*killer*/)
-        {
-            // always spawn spirit on death
-            // if totem around
-            // move spirit to totem and cast kill count
-            uint32 entry = 0;
-            uint32 credit = 0;
-
-            switch (me->GetEntry())
-            {
-                  case NPC_ENRAGED_FIRE_SPIRIT:
-                    entry  = NPC_FIERY_SOUL;
-                    //credit = SPELL_FIERY_SOUL_CAPTURED_CREDIT;
-                    credit = NPC_CREDIT_FIRE;
-                    break;
-                  case NPC_ENRAGED_EARTH_SPIRIT:
-                    entry  = NPC_EARTHEN_SOUL;
-                    //credit = SPELL_EARTHEN_SOUL_CAPTURED_CREDIT;
-                    credit = NPC_CREDIT_EARTH;
-                    break;
-                  case NPC_ENRAGED_AIR_SPIRIT:
-                    entry  = NPC_ENRAGED_AIRY_SOUL;
-                    //credit = SPELL_AIRY_SOUL_CAPTURED_CREDIT;
-                    credit = NPC_CREDIT_AIR;
-                    break;
-                  case NPC_ENRAGED_WATER_SPIRIT:
-                    entry  = NPC_ENRAGED_WATERY_SOUL;
-                    //credit = SPELL_WATERY_SOUL_CAPTURED_CREDIT;
-                    credit = NPC_CREDIT_WATER;
-                    break;
-                default:
-                    break;
-            }
-
-            // Spawn Soul on Kill ALWAYS!
-            Creature* Summoned = NULL;
-            Unit* totemOspirits = NULL;
-
-            if (entry != 0)
-                Summoned = DoSpawnCreature(entry, 0, 0, 1, 0, TEMPSUMMON_TIMED_OR_DEAD_DESPAWN, 5000);
-
-            // FIND TOTEM, PROCESS QUEST
-            if (Summoned)
-            {
-                 totemOspirits = me->FindNearestCreature(ENTRY_TOTEM_OF_SPIRITS, RADIUS_TOTEM_OF_SPIRITS);
-                 if (totemOspirits)
-                 {
-                     Summoned->setFaction(FACTION_ENRAGED_SOUL_FRIENDLY);
-                     Summoned->GetMotionMaster()->MovePoint(0, totemOspirits->GetPositionX(), totemOspirits->GetPositionY(), Summoned->GetPositionZ());
-
-                     if (Unit* owner = totemOspirits->GetOwner())
-                         if (Player* player = owner->ToPlayer())
-                             player->KilledMonsterCredit(credit, 0);
-                     DoCast(totemOspirits, SPELL_SOUL_CAPTURED);
-                 }
-            }
-        }
-    };
-};
-
 enum ShadowMoonTuberEnum
 {
     SPELL_WHISTLE               = 36652,
@@ -1991,7 +1865,6 @@ void AddSC_shadowmoon_valley()
     new go_crystal_prison();
     new npc_illidari_spawn();
     new npc_torloth_the_magnificent();
-    new npc_enraged_spirit();
     new npc_shadowmoon_tuber_node();
 }
 
