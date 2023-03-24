94ca2dae09e28aeb2342de0000cb5e3c78678611
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2023-03-24 17:12:14 +0100

    Sol: Fix SAI for Sundered Rumbler
    Related commit: 4dbe5a37d507bbbe6292ca077adf288f34c01057

---
+++ data/sql/updates/db_world/2023_03_24_00.sql
@@ -0,0 +1,2 @@
+UPDATE `smart_scripts` SET `link` = 0 WHERE `entryorguid` = 18881 AND `source_type` = 0 AND `id` = 1;
+UPDATE `smart_scripts` SET `event_type` = 6, `comment` = 'Sundered Rumbler - On Death - Cast ''Summon Sundered Shard''' WHERE `entryorguid` = 18881 AND `source_type` = 0 AND `id` = 2;
