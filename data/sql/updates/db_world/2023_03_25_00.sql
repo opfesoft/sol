a624b0e03a53225f7f1b3b19a098b9b165475c76
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2023-03-25 15:18:03 +0100

    Sol: Improve SAI for Mosh'Ogg Spellcrafter
    Based on AC commit 648d54fa195447c25cb65b4f1f1fffb28fec22d3

---
+++ data/sql/updates/db_world/2023_03_25_00.sql
@@ -0,0 +1,2 @@
+UPDATE `smart_scripts` SET `event_param1` = 0, `event_param2` = 0, `event_param3` = 3400, `event_param4` = 4800 WHERE `entryorguid` = 710 AND `source_type` = 0 AND `id` = 1;
+UPDATE `smart_scripts` SET `event_param1` = 5000, `event_param2` = 9000, `event_param3` = 12000, `event_param4` = 15000 WHERE `entryorguid` = 710 AND `source_type` = 0 AND `id` = 2;
