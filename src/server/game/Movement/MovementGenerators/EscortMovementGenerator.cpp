98556b945a45228bba8301df4de4b1e927499181
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2023-04-28 10:39:03 +0200

    Sol: Update license info for EscortMovementGenerator

--- src/server/game/Movement/MovementGenerators/EscortMovementGenerator.cpp
+++ src/server/game/Movement/MovementGenerators/EscortMovementGenerator.cpp
@@ -1,6 +1,7 @@
 /*
-Written by Xinef
- */
+ * Copyright (C) 2020+     Project "Sol" <https://gitlab.com/opfesoft/sol>, released under the GNU AGPLv3 license: https://gitlab.com/opfesoft/sol/-/blob/master/LICENSE.md
+ * Originally written by Xinef - Copyright (C) 2016-2020 AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
+*/
 
 #include "EscortMovementGenerator.h"
 #include "Errors.h"
