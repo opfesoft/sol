bc9f3c9975192c4967c1ae036dabf70e286ccda1
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2023-05-22 23:46:28 +0200

    Sol: Add aura "Invisibility and Stealth Detection" for a few mobs
    - Based on AC commit 08308230ef28dd2a15fade366feee66e5a410e1c
    - Affected creatures: Phantom Valet, Spectral Sentry, Bloodmaul Dire Wolf

---
+++ data/sql/updates/db_world/2023_05_22_00.sql
@@ -0,0 +1,11 @@
+DELETE FROM `creature_template_addon` WHERE `entry` = 16408;
+INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `isLarge`, `auras`)
+VALUES
+(16408,0,0,0,1,0,0,'18950');
+
+UPDATE `creature_template_addon` SET `auras` = '18950' WHERE `entry` IN (16424,20058);
+
+UPDATE `creature_addon` SET `auras` = '18950' WHERE `guid` IN (
+135170,135171,135174,135175,135345,135346,135347,135348,135349,135350,135351,135352,135353,135354,135355,135356,
+135357,135358,135359,135360,135361,135362,71502,71503,71504,71505,71506,71507,71508,71509,71510,71511,
+71512,71513,71514,71515);
