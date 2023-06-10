0eaecba1b4b7c0720ab730733ce916cbd918981e
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2023-06-10 12:22:47 +0200

    Sol: Improve SAI for Thule Ravenclaw
    Based on AC commit ac45b552b256bbc058bef68990806281a28ea82a

---
+++ data/sql/updates/db_world/2023_06_10_00.sql
@@ -0,0 +1,2 @@
+UPDATE `smart_scripts` SET `action_param2` = 0 WHERE `entryorguid` = 1947 AND `source_type` = 0 AND `id` = 0;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 1947 AND `source_type` = 0 AND `id` > 3;
