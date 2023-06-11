840122246e37e746218d471233856a96acf36746
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2023-06-11 17:14:13 +0200

    Sol: Increase OOC talk intervals for a few mobs in Darkshire

---
+++ data/sql/updates/db_world/2023_06_11_02.sql
@@ -0,0 +1,3 @@
+UPDATE `smart_scripts` SET `event_param1` = 600000, `event_param2` = 1200000, `event_param3` = 600000, `event_param4` = 1200000 WHERE `entryorguid` IN (269,270,271) AND `source_type` = 0 AND `id` = 0;
+UPDATE `smart_scripts` SET `event_param1` = 600000, `event_param2` = 1200000, `event_param3` = 600000, `event_param4` = 1200000 WHERE `entryorguid` IN (263,264) AND `source_type` = 0 AND `id` = 1;
+UPDATE `smart_scripts` SET `event_param1` = 300000, `event_param2` = 600000, `event_param3` = 300000, `event_param4` = 600000 WHERE `entryorguid` = 325 AND `source_type` = 0 AND `id` = 0;
