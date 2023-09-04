2dbae40f8676ccc836a1f0412694b310546d22af
Stoabrogga <stoabrogga@proton.me>
2023-09-04 16:03:34 +0200

    Sol: Improve SAI for Nestlewood Owlkin
    First use of SMART_EVENT_CHANNEL_FINISHED_TARGET (see commit ccde37d9a5c3e1148b6bc5da9a6b70c30ae86794)

---
+++ data/sql/updates/db_world/2023_09_04_02.sql
@@ -0,0 +1,4 @@
+DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` = 16518;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`)
+VALUES
+(16518,0,0,0,238,0,100,0,29528,0,15000,15000,0,80,1651800,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Nestlewood Owlkin - On Channel Finished Target ''Inoculate Nestlewood Owlkin'' - Call Timed Action List');
