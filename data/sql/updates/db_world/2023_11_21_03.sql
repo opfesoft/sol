62656011f0b13eee335d14f5c9a2cc6930261f04
Stoabrogga <stoabrogga@proton.me>
2023-11-21 22:30:15 +0100

    Sol: Improve quest "Grim Message"
    Based on AC commit 3228d7d762fd3490d8b0dbf72a8a5df2ff75199a

---
+++ data/sql/updates/db_world/2023_11_21_03.sql
@@ -0,0 +1,4 @@
+DELETE FROM `gameobject` WHERE `guid` IN (99873,99874);
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `VerifiedBuild`)
+VALUES
+(99873,142698,0,0,0,1,1,-24.2883,-2468.25,122.442,0,0,0,0,0,180,0,1,0);
