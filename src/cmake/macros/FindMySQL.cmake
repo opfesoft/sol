321f6dc88a6c88305eacf78e4ea627e697440963
Stoabrogga <stoabrogga@proton.me>
2023-12-09 17:06:44 +0100

    Sol: Rework FindMySQL.cmake

--- src/cmake/macros/FindMySQL.cmake
+++ src/cmake/macros/FindMySQL.cmake
@@ -4,20 +4,12 @@
 
 # This module defines
 # MYSQL_INCLUDE_DIR, where to find mysql.h
-# MYSQL_LIBRARIES, the libraries to link against to connect to MySQL
-# MYSQL_FOUND, if false, you cannot build anything that requires MySQL.
-
-# also defined, but not for general use are
-# MYSQL_LIBRARY, where to find the MySQL library.
+# MYSQL_LIBRARY, the libraries to link against to connect to MariaDB
+# MYSQL_FOUND, if false, you cannot build anything that requires MariaDB
 
 set( MYSQL_FOUND 0 )
 
-set(MYSQL_CONFIG_PREFER_PATH "$ENV{MYSQL_HOME}/bin" CACHE FILEPATH
-  "preferred path to MySQL (mariadb_config)"
-)
-
 find_program(MYSQL_CONFIG mariadb_config
-  ${MYSQL_CONFIG_PREFER_PATH}
   /usr/local/bin/
   /usr/bin/
 )
@@ -25,38 +17,18 @@ find_program(MYSQL_CONFIG mariadb_config
 if( MYSQL_CONFIG )
   message(STATUS "Using mariadb_config: ${MYSQL_CONFIG}")
   # set INCLUDE_DIR
-  exec_program(${MYSQL_CONFIG}
-    ARGS --include
-    OUTPUT_VARIABLE MY_TMP
+  execute_process(COMMAND ${MYSQL_CONFIG} --variable=pkgincludedir
+    OUTPUT_VARIABLE MYSQL_ADD_INCLUDE_PATH
+    OUTPUT_STRIP_TRAILING_WHITESPACE
   )
 
-  string(REGEX REPLACE "-I([^ ]*)( .*)?" "\\1" MY_TMP "${MY_TMP}")
-  set(MYSQL_ADD_INCLUDE_PATH ${MY_TMP} CACHE FILEPATH INTERNAL)
-  #message("[DEBUG] MYSQL ADD_INCLUDE_PATH : ${MYSQL_ADD_INCLUDE_PATH}")
+  #message("[DEBUG] MARIADB ADD_INCLUDE_PATH : ${MYSQL_ADD_INCLUDE_PATH}")
   # set LIBRARY_DIR
-  exec_program(${MYSQL_CONFIG}
-    ARGS --libs_r
-    OUTPUT_VARIABLE MY_TMP
+  execute_process(COMMAND ${MYSQL_CONFIG} --variable=pkglibdir
+    OUTPUT_VARIABLE MYSQL_ADD_LIBRARIES_PATH
+    OUTPUT_STRIP_TRAILING_WHITESPACE
   )
-  set(MYSQL_ADD_LIBRARIES "")
-  string(REGEX MATCHALL "-l[^ ]*" MYSQL_LIB_LIST "${MY_TMP}")
-  foreach(LIB ${MYSQL_LIB_LIST})
-    string(REGEX REPLACE "[ ]*-l([^ ]*)" "\\1" LIB "${LIB}")
-    list(APPEND MYSQL_ADD_LIBRARIES "${LIB}")
-    #message("[DEBUG] MYSQL ADD_LIBRARIES : ${MYSQL_ADD_LIBRARIES}")
-  endforeach(LIB ${MYSQL_LIB_LIST})
-
-  set(MYSQL_ADD_LIBRARIES_PATH "")
-  string(REGEX MATCHALL "-L[^ ]*" MYSQL_LIBDIR_LIST "${MY_TMP}")
-  foreach(LIB ${MYSQL_LIBDIR_LIST})
-    string(REGEX REPLACE "[ ]*-L([^ ]*)" "\\1" LIB "${LIB}")
-    list(APPEND MYSQL_ADD_LIBRARIES_PATH "${LIB}")
-    #message("[DEBUG] MYSQL ADD_LIBRARIES_PATH : ${MYSQL_ADD_LIBRARIES_PATH}")
-  endforeach(LIB ${MYSQL_LIBS})
-
-else( MYSQL_CONFIG )
-  set(MYSQL_ADD_LIBRARIES "")
-  list(APPEND MYSQL_ADD_LIBRARIES "mysqlclient_r")
+  #message("[DEBUG] MARIADB ADD_LIBRARIES_PATH : ${MYSQL_ADD_LIBRARIES_PATH}")
 endif( MYSQL_CONFIG )
 
 find_path(MYSQL_INCLUDE_DIR
@@ -64,49 +36,25 @@ find_path(MYSQL_INCLUDE_DIR
     mysql.h
   PATHS
     ${MYSQL_ADD_INCLUDE_PATH}
-    /usr/include
-    /usr/include/mysql
-    /usr/local/include
-    /usr/local/include/mysql
-    /usr/local/mysql/include
-  DOC
-    "Specify the directory containing mysql.h."
+  DOC "Specify the directory containing mysql.h"
 )
 
-foreach(LIB ${MYSQL_ADD_LIBRARIES})
-  find_library( MYSQL_LIBRARY 
-    NAMES
-      mysql libmysql ${LIB}
-    PATHS
-      ${MYSQL_ADD_LIBRARIES_PATH}
-      /usr/lib
-      /usr/lib/mysql
-      /usr/local/lib
-      /usr/local/lib/mysql
-      /usr/local/mysql/lib
-    DOC "Specify the location of the mysql library here."
-  )
-endforeach(LIB ${MYSQL_ADD_LIBRARY})
-
-find_library( MYSQL_EXTRA_LIBRARIES
+find_library( MYSQL_LIBRARY
   NAMES
-    z zlib
+    mariadb
   PATHS
-    /usr/lib
-    /usr/local/lib
-  DOC
-    "if more libraries are necessary to link in a MySQL client (typically zlib), specify them here."
-  )
+    ${MYSQL_ADD_LIBRARIES_PATH}
+  DOC "Specify the location of the mariadb library here"
+)
 
 if( MYSQL_LIBRARY )
   if( MYSQL_INCLUDE_DIR )
     set( MYSQL_FOUND 1 )
-    message(STATUS "Found MySQL library: ${MYSQL_LIBRARY}")
-    message(STATUS "Found MySQL headers: ${MYSQL_INCLUDE_DIR}")
+    message(STATUS "Found MariaDB library: ${MYSQL_LIBRARY}")
+    message(STATUS "Found MariaDB headers: ${MYSQL_INCLUDE_DIR}")
   else( MYSQL_INCLUDE_DIR )
-    message(FATAL_ERROR "Could not find MySQL headers! Please install the development libraries and headers")
+    message(FATAL_ERROR "Could not find MariaDB headers! Please install the development libraries and headers")
   endif( MYSQL_INCLUDE_DIR )
-  mark_as_advanced( MYSQL_FOUND MYSQL_LIBRARY MYSQL_EXTRA_LIBRARIES MYSQL_INCLUDE_DIR )
 else( MYSQL_LIBRARY )
-  message(FATAL_ERROR "Could not find the MySQL libraries! Please install the development libraries and headers")
+  message(FATAL_ERROR "Could not find the MariaDB libraries! Please install the development libraries and headers")
 endif( MYSQL_LIBRARY )
