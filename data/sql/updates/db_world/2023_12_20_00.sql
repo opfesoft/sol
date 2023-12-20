c444a060473c51012bfa38a66b35f418efab3971
Stoabrogga <stoabrogga@proton.me>
2023-12-20 14:53:05 +0100

    Sol: Adjust Obsidia encounter
    Related Sol commit: c46ad425d780d60945e1ec1e435357044764dbcb

---
+++ data/sql/updates/db_world/2023_12_20_00.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_template` SET `minlevel` = 72 WHERE `entry` = 23282;
+UPDATE `creature` SET `position_x` = 2011.64, `position_y` = 7213.34, `position_z` = 497.217, `orientation` = 0.0361174 WHERE `guid` = 3009669;
+UPDATE `smart_scripts` SET `target_x` = 2011.64, `target_y` = 7213.34 WHERE `source_type` = 1 AND `entryorguid` = 185932 AND `id` = 0;
