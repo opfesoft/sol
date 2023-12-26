9595438b6de36a78f8138be6c200122385e37c54
Stoabrogga <stoabrogga@proton.me>
2023-12-26 16:31:01 +0100

    Sol: Fix talent Static Shock
    Based on AC commit 8b4596e572e0c1232407d0773b16ac7959c4d98b

---
+++ data/sql/updates/db_world/2023_12_26_00.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_proc_event` WHERE `entry` = -51525;
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`)
+VALUES
+(-51525,0,0,0,0,0,12582912,0,0,0,0);
