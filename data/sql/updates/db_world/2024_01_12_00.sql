66c8e8aa6441d02a8a411a55f10fa6d07cc5472b
Stoabrogga <stoabrogga@proton.me>
2024-01-12 12:09:44 +0100

    Sol: Add spawn for Blazzle
    Based on AC commit 3a07dffe9df19937d6d80c81fabb2232ef99afa2

---
+++ data/sql/updates/db_world/2024_01_12_00.sql
@@ -0,0 +1,4 @@
+DELETE FROM `creature` WHERE `guid` = 3009673;
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`)
+VALUES
+(3009673,28344,530,0,0,1,1,0,0,3063.41,3677.57,142.761,4.27606,300,0,1,0,0,0,0,0);
