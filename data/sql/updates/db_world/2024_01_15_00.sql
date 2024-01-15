5e27ac5fefa33ec31e560b0c37f8ed3ed32209e9
Stoabrogga <stoabrogga@proton.me>
2024-01-15 13:12:49 +0100

    Sol: Update position for GO "Draconic for Dummies"
    Based on AC commit da63cc0c6137a637da505b8031083226f60b698b

---
+++ data/sql/updates/db_world/2024_01_15_00.sql
@@ -0,0 +1,1 @@
+UPDATE `gameobject` SET `position_x` = -8340.2, `position_y` = 413.75, `position_z` = 124.49, `orientation` = 2.82743, `rotation2` = 0.987688, `rotation3` = 0.156436 WHERE `guid` = 268645;
