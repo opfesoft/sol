63370ccebfcda50a36e509d8c5c18f81ccca9384
Stoabrogga <stoabrogga@proton.me>
2024-01-17 23:18:20 +0100

    Sol: Fix broadcast text 31559
    Orgrimmar guard directions for the barber (old text was from Cataclysm)

---
+++ data/sql/updates/db_world/2024_01_17_03.sql
@@ -0,0 +1,2 @@
+UPDATE `broadcast_text` SET `MaleText` = 'The barber is in the Cleft of Shadow.', `FemaleText` = 'The barber is in the Cleft of Shadow.' WHERE `ID` = 31559;
+UPDATE `broadcast_text_locale` SET `MaleText` = 'Der Barbier ist in der Kluft der Schatten.', `FemaleText` = 'Der Barbier ist in der Kluft der Schatten.' WHERE `ID` = 31559 AND `locale` = 'deDE';
