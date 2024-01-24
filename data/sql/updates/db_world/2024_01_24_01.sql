8ac3db09495cfc60a2f3056b5797eb163d43e73a
Stoabrogga <stoabrogga@proton.me>
2024-01-24 23:09:17 +0100

    Sol: Fix SAI scripts concerning GO despawn

---
+++ data/sql/updates/db_world/2024_01_24_01.sql
@@ -0,0 +1,3 @@
+UPDATE `smart_scripts` SET `action_type` = 233, `action_param1` = 0 WHERE `source_type` = 1 AND `entryorguid` = 179485 AND `id` = 0;
+UPDATE `smart_scripts` SET `action_type` = 233 WHERE `source_type` = 1 AND `entryorguid` = 188463 AND `id` = 1;
+UPDATE `smart_scripts` SET `action_type` = 233 WHERE `source_type` = 9 AND `entryorguid` = 2584900 AND `id` IN (2,3,4);
