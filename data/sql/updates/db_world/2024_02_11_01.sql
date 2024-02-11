8f97e7152776ddfb41835ed613ce838e93d29e82
Stoabrogga <stoabrogga@proton.me>
2024-02-11 22:37:02 +0100

    Sol: Adjust position/movement for Darkcrest Siren

---
+++ data/sql/updates/db_world/2024_02_11_01.sql
@@ -0,0 +1,1 @@
+UPDATE `creature` SET `position_x` = -424.805, `position_y` = 6287.87, `position_z` = 19.8855, `orientation` = 2.62691, `wander_distance` = 5, `MovementType` = 1 WHERE `guid` = 63974;
