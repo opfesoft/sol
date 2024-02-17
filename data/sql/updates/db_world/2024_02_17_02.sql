2e36b6490f4960f0392b30c485c81f73600569d0
Stoabrogga <stoabrogga@proton.me>
2024-02-17 17:09:12 +0100

    Sol: Improve SAI for Ol' Beasley

---
+++ data/sql/updates/db_world/2024_02_17_02.sql
@@ -0,0 +1,1 @@
+UPDATE `smart_scripts` SET `event_param1` = 180000, `event_param2` = 360000, `event_param3` = 180000, `event_param4` = 360000 WHERE `source_type` = 0 AND `entryorguid` = 1395 AND `id` = 0;
