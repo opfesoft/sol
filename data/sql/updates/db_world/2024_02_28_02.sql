46c41beeb0b8fbe9548e259a79ad49ab9b338d2c
Stoabrogga <stoabrogga@proton.me>
2024-02-28 23:07:12 +0100

    Sol: Adjust position & unit flags for a few NPCs in the Champions' Hall

---
+++ data/sql/updates/db_world/2024_02_28_02.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_template` SET `unit_flags` = `unit_flags` | 768 WHERE `entry` IN (34081,40607,34078,34075);
+UPDATE `creature` SET `position_x` = -8768.01, `position_y` = 418.516, `position_z` = 103.921 WHERE `guid` = 133918;
