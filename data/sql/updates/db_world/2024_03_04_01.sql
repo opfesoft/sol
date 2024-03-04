547f982613a1e6116f0e3aac0127c88b2cb1f28a
Stoabrogga <stoabrogga@proton.me>
2024-03-04 14:12:20 +0100

    Sol: Add Barber Chair to Cosmowrench
    Position taken from AC commit 8936a4aacd2e1b09c1bf5247d4331de87e4163c3

---
+++ data/sql/updates/db_world/2024_03_04_01.sql
@@ -0,0 +1,4 @@
+DELETE FROM `gameobject` WHERE `guid` = 3009238;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+(3009238,191817,530,0,0,1,1,2969.09,1773.89,139.69,5.36592,0,0,-0.442723,0.896658,120,255,1);
