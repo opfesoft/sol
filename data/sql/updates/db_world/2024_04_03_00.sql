11e05b0ad76fe975dd5c2194bde34895e72a74c2
Stoabrogga <stoabrogga@proton.me>
2024-04-03 22:27:10 +0200

    Sol: Improve movement/position for several satyrs in the Bloodmyst Isle area

---
+++ data/sql/updates/db_world/2024_04_03_00.sql
@@ -0,0 +1,6 @@
+UPDATE `creature` SET `wander_distance` = 0, `MovementType` = 0 WHERE `guid` IN (62355,62361,62365,62366,62368,62373,62378,62379,62384,62385,62389,62395,62399,62400,62403,62404,62420,62433,62436);
+UPDATE `creature` SET `wander_distance` = 5, `MovementType` = 1 WHERE `guid` IN (62356,62423);
+UPDATE `creature` SET `position_x` = -2339.99, `position_y` = -11352.1, `position_z` = 29.8465 WHERE `guid` = 62388;
+UPDATE `creature` SET `position_x` = -2564.1, `position_y` = -11267.6, `position_z` = 34.6193 WHERE `guid` = 62391;
+UPDATE `creature` SET `position_x` = -2511.19, `position_y` = -11258.7, `position_z` = 36.1675 WHERE `guid` = 86546;
+UPDATE `creature` SET `position_x` = -2322.49, `position_y` = -11187.6, `position_z` = 14.0072 WHERE `guid` = 86553;
