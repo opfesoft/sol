a699031e73f6cfe563b53476075e960311095e55
Stoabrogga <stoabrogga@proton.me>
2024-04-22 15:56:15 +0200

    Sol: Add "A Dusty Tome" spawn
    Based on AC commit 1c61246994b8541f07933118f4dbf851f39e1fd8

---
+++ data/sql/updates/db_world/2024_04_22_01.sql
@@ -0,0 +1,4 @@
+DELETE FROM `gameobject` WHERE `guid` = 3009350;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+(3009350,179548,429,0,0,1,1,4.49781,-437.567,16.4125,4.13643,0,0,-0.878817,0.47716,7200,255,1);
