9775c20e864c1a41f0b37ee1cc38f4824000d4d8
Stoabrogga <stoabrogga@proton.me>
2024-04-22 13:45:35 +0200

    Sol: Improve position for a few NPCs in Timbermaw Hold

---
+++ data/sql/updates/db_world/2024_04_22_00.sql
@@ -0,0 +1,6 @@
+UPDATE `creature` SET `orientation` = 5.78879 WHERE `guid` = 39717;
+UPDATE `creature` SET `orientation` = 1.79897 WHERE `guid` = 39718;
+UPDATE `creature` SET `orientation` = 5.46286 WHERE `guid` = 39726;
+UPDATE `creature` SET `position_x` = 7012.68, `position_y` = -2099.29, `position_z` = 602.057, `orientation` = 0.239954 WHERE `guid` = 39725;
+UPDATE `creature` SET `orientation` = 2.61186 WHERE `guid` = 39729;
+UPDATE `creature` SET `position_x` = 6952.38, `position_y` = -2267.49, `position_z` = 591.433, `orientation` = 1.58298 WHERE `guid` = 40366;
