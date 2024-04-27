22854af735e8e83152950ba2a0b7e8a60e6ba707
Stoabrogga <stoabrogga@proton.me>
2024-04-27 10:05:49 +0200

    Sol: Use intermediate paths for Sahvan Bloodshadow

---
+++ data/sql/updates/db_world/2024_04_27_00.sql
@@ -0,0 +1,2 @@
+UPDATE `waypoint_data` SET `pathfinding` = 4 WHERE `id` = 319190;
+UPDATE `waypoint_data` SET `pathfinding` = 3 WHERE `id` = 319190 AND `point` IN (1,11,14,15,16,19,20,21,24,25,26,29,32,35,43,47,53,59,66,73,79);
