1f72ec82451204398346c34188ddde9275b1e515
Stoabrogga <stoabrogga@proton.me>
2024-06-28 16:34:07 +0200

    Sol: Adjust position for 2 chickens in Southshore
    Based on AC commit 7a99e0ba82db88da2b632d701acdacb900b0b8be

---
+++ data/sql/updates/db_world/2024_06_28_00.sql
@@ -0,0 +1,2 @@
+UPDATE `creature` SET `position_x` = -814.344, `position_y` = -583.08, `position_z` = 15.1515 WHERE `guid` = 15403;
+UPDATE `creature` SET `position_x` = -752.313, `position_y` = -550.332, `position_z` = 18.978 WHERE `guid` = 15943;
